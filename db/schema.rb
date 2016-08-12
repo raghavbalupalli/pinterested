ae9f74db2891ce15cec7e56b50af57c0214d6c94
raghavbalupalli <raghavbalupalli@gmail.com>
2016-08-11 21:31:45 -0700

    added pins and navbar changed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812005027) do
+ActiveRecord::Schema.define(version: 20160812015749) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string   "description", limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
